7bbe8af127c54ff217fc99a81503dcbd11477d50
SayanPark <s.poormanesh2002@gmail.com>
2025-07-24 22:11:17 +0330

    modified:   backend/settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -169,6 +169,8 @@ FROM_EMAIL = "no-reply@yourdomain.com"
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
